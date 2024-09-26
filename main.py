75db6dd4f8c84d19bf013b11447851762a738d1c
Vb267 <vrushankbhosale@gmail.com>
2024-09-27 00:25:59 +0530

    Base setup Backend

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from models import Property
+from database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/properties/")
+def read_properties(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    properties = db.query(Property).offset(skip).limit(limit).all()
+    return properties
---
+++ models.py
@@ -0,0 +1,13 @@
+# models.py
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Property(Base):
+    __tablename__ = 'properties'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    location = Column(String)
+    image_url = Column(String)
