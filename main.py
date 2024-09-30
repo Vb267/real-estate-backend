e3f6af9e1b977ab0da8d0caf9491ed51e0e83dee
Vb267 <vrushankbhosale@gmail.com>
2024-09-30 18:03:09 +0530

    Authentication added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
 import models, schemas
 from database import SessionLocal, engine
+from utils import hash_password, verify_password
+from auth import create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
+
 
 app = FastAPI()
 
@@ -72,3 +76,31 @@ def delete_property(property_id: int, db: Session = Depends(get_db)):
     db.delete(db_property)
     db.commit()
     return {"detail": "Property deleted successfully"}
+
+
+@app.post("/register/")
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = hash_password(user.password)
+    db_user = models.User(
+        username=user.username, email=user.email, hashed_password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return {"message": "User registered successfully"}
+
+
+@app.post("/login/", response_model=schemas.Token)
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user = (
+        db.query(models.User).filter(models.User.username == form_data.username).first()
+    )
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
+        )
+
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
