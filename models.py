8d5e9c059794f7ae0571947fd3b7094adc596c6a
Vb267 <vrushankbhosale@gmail.com>
2024-09-30 17:26:35 +0530

    Users added

--- models.py
+++ models.py
@@ -12,3 +12,12 @@ class Property(Base):
     location = Column(String)
     size = Column(Float)
     image_url = Column(String)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
--- schemas.py
+++ schemas.py
@@ -19,3 +19,23 @@ class Property(PropertyBase):
 
     class Config:
         orm_mode = True
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
