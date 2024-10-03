b212c8da646ec3d97dcb52e6ea48614711ea6216
Vb267 <vrushankbhosale@gmail.com>
2024-10-03 17:43:58 +0530

    Added schemas

---
+++ app/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+
+
+class ListingBase(BaseModel):
+    title: str
+    description: str
+    price: float
+    location: str
+
+
+class ListingCreate(ListingBase):
+    pass
+
+
+class Listing(ListingBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
